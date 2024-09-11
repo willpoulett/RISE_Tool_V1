b5ce06ec27caa44317c6908f64d5cc319063837a
Will Poulett <william.poulett1@nhs.net>
2024-09-11 08:10:29 +0100

    add sort

--- utils/utils.py
+++ utils/utils.py
@@ -7,6 +7,7 @@ def init(file_names = ["edge_case_images","non_edge_case_images"]):
     for folder in file_names:
         fileList = glob.glob(f'{folder}/*')
         image_paths.extend(fileList)
-
-
-    return image_paths.sort()
+        
+    image_paths.sort
+        
+    return image_paths
